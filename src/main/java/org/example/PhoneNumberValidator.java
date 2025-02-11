bdd80eea2bb417b657e4fb9b949a8246af7825d0
honda <nvymoon@gmail.com>
2025-02-11 19:36:55 +0900

    更新

--- src/main/java/org/example/PhoneNumberValidator.java
+++ src/main/java/org/example/PhoneNumberValidator.java
@@ -2,17 +2,25 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Main {
+public class PhoneNumberValidator {
 
   public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
+    String phoneNumber;
 
-    System.out.println("Enter your phone number: ");
+    while (true) {
+      System.out.println("Enter your phone number: ");
+      phoneNumber = scanner.nextLine();
 
-    String phoneNumber = scanner.nextLine();
+      if (phoneNumber.matches("^0[789]0-\\d{4}-\\d{4}$")) {
+        System.out.println(phoneNumber + ": Phone number is valid");
+        break;
+      } else {
+        System.out.println(phoneNumber + ": Phone number is not valid. Please try again.");
+      }
+    }
 
-    System.out.println(phoneNumber + "は有効な携帯電話番号です。");
-
+    scanner.close();
   }
 }
